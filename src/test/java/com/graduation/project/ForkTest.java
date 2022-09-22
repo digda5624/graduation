4718ed3a7a0ea2ae6652bb5f3e03f582a1d48770
JUNYEONG-K <godoly1211@naver.com>
2022-09-22 17:02:52 +0900

    fork test

---
+++ src/test/java/com/graduation/project/ForkTest.java
@@ -0,0 +1,10 @@
+package com.graduation.project;
+
+import org.junit.jupiter.api.Test;
+
+public class ForkTest {
+    @Test
+    void ForkTest() {
+        System.out.println("졸프 fork 해서 pull request 보내기");
+    }
+}
